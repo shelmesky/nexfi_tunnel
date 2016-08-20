69aab632791c58ec4f2c9548aa8c1f346d10c22e
roy.lieu <roy.lieu@gmail.com>
2016-08-20 19:02:32 +0800

    first commit

---
+++ client.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"github.com/songgao/water"
+	"io"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
+var (
+	encoder *gob.Encoder
+	decoder *gob.Decoder
+
+	wg sync.WaitGroup
+)
+
+type ClientData struct {
+	DestAddr string
+	Data     []byte
+}
+
+const BUFFERSIZE = 1528
+
+func main() {
+	log.Println("Client start")
+
+	if len(os.Args) < 3 {
+		log.Println("Please spicify server address and port")
+		return
+	}
+
+	server_addr := fmt.Sprintf("%s:%s", os.Args[1], os.Args[2])
+
+	server_conn, err := net.DialTimeout("tcp", server_addr, 3*time.Second)
+	if err != nil {
+		log.Println("cannot connect to server:", err)
+		return
+	}
+
+	iface, err := water.NewTAP("")
+	if err != nil {
+		log.Println("Create new TAP interface failed:", err)
+		return
+	}
+
+	encoder = gob.NewEncoder(server_conn)
+	decoder = gob.NewDecoder(server_conn)
+
+	go ConnToIface(server_conn, iface)
+	go IfaceToConn(server_conn, iface)
+
+	wg.Add(2)
+	wg.Wait()
+}
+
+func ConnToIface(conn net.Conn, iface *water.Interface) {
+	defer wg.Done()
+	for {
+		client_data := new(ClientData)
+
+		err := decoder.Decode(client_data)
+		if err == io.EOF {
+			log.Println("Client connection close")
+			conn.Close()
+			iface.Close()
+			break
+		}
+
+		if err != nil {
+			log.Println("decode data failed:", err)
+			conn.Close()
+			iface.Close()
+			break
+		}
+
+		_, err = iface.Write(client_data.Data)
+
+		if err != nil {
+			log.Println("Write data to NIC failed:", err)
+			conn.Close()
+			iface.Close()
+			break
+		}
+	}
+}
+
+func IfaceToConn(conn net.Conn, iface *water.Interface) {
+	defer wg.Done()
+	buffer := make([]byte, BUFFERSIZE)
+	for {
+		_, err := iface.Read(buffer)
+		if err != nil {
+			log.Println("Read from NIC failed:", err)
+			iface.Close()
+			conn.Close()
+			break
+		}
+
+		client_data := new(ClientData)
+		client_data.DestAddr = "client"
+		client_data.Data = buffer
+
+		err = encoder.Encode(client_data)
+		if err != nil {
+			log.Println("Send data to client failed:", err)
+			iface.Close()
+			conn.Close()
+			break
+		}
+	}
+}
---
+++ server.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/gob"
+	"github.com/songgao/water"
+	//"github.com/songgao/water/waterutil"
+	"io"
+	"log"
+	"net"
+	"sync"
+)
+
+var (
+	encoder *gob.Encoder
+	decoder *gob.Decoder
+
+	wg sync.WaitGroup
+)
+
+type ClientData struct {
+	DestAddr string
+	Data     []byte
+}
+
+const BUFFERSIZE = 1528
+const SERVER_ADDR = "0.0.0.0:9199"
+
+func main() {
+	log.Println("Start server")
+
+	server_sock, err := net.Listen("tcp", SERVER_ADDR)
+
+	if err != nil {
+		log.Println("can not listen on tcp:", err)
+		return
+	}
+
+	log.Println("Server listen:", SERVER_ADDR)
+
+	for {
+		conn, err := server_sock.Accept()
+		if err != nil {
+			continue
+		}
+
+		encoder = gob.NewEncoder(conn)
+		decoder = gob.NewDecoder(conn)
+
+		iface, err := water.NewTAP("")
+
+		go ConnToIface(conn, iface)
+		go IfaceToConn(conn, iface)
+		wg.Add(2)
+	}
+
+	wg.Wait()
+}
+
+func ConnToIface(conn net.Conn, iface *water.Interface) {
+	defer wg.Done()
+	for {
+		client_data := new(ClientData)
+		err := decoder.Decode(client_data)
+		if err == io.EOF {
+			log.Println("Client connection close")
+			conn.Close()
+			break
+		}
+
+		if err != nil {
+			log.Println("decode data failed:", err)
+			conn.Close()
+			break
+		}
+
+		_, err = iface.Write(client_data.Data)
+
+		if err != nil {
+			log.Println("Write data to NIC failed:", err)
+			conn.Close()
+			iface.Close()
+		}
+	}
+}
+
+func IfaceToConn(conn net.Conn, iface *water.Interface) {
+	defer wg.Done()
+	buffer := make([]byte, BUFFERSIZE)
+	for {
+		_, err := iface.Read(buffer)
+		if err != nil {
+			log.Println("Read from NIC failed:", err)
+			iface.Close()
+			conn.Close()
+			break
+		}
+
+		client_data := new(ClientData)
+		client_data.DestAddr = "client"
+		client_data.Data = buffer
+
+		encoder.Encode(client_data)
+		err = encoder.Encode(client_data)
+		if err != nil {
+			log.Println("Send data to client failed:", err)
+			iface.Close()
+			conn.Close()
+			break
+		}
+
+	}
+}
